1c154306fe2d5489fd72dc9bb4227c0f7d71a0e8
notmsc <mscaro22@gmail.com>
2019-06-30 23:46:10 -0700

    added state change on click of nav bar to change class-name

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,12 +3,34 @@ import '../../App.css';
 import './styles.css';
 import './hamburger.css';
 
+
+
 class Nav extends React.Component {
+    //==== This controls Nav state ====
+    constructor(props) {
+        super(props);
+        this.state = {
+            ishamburgeractive: true,
+        }
+
+        this.hamburgercross = this.hamburgercross.bind(this);         
+    }
+    
+    //==== This controls hamburger cross ====
+    hamburgercross() {
+        this.setState(updateState => ({
+            ishamburgeractive: !updateState.ishamburgeractive
+        }));
+    }
+
     render() {
+        //==== create variables and logic before the return statement ====
+        let hamburgerContainerName = this.state.ishamburgeractive ? 'hamburger-container' : 'hamburger-container hamburger-cross';
+
         return(
             <div className={'navContainer'}>
-                <div className="position-hamburger">
-                    <a className="hamburger-container">
+                <div className={"position-hamburger"}>
+                    <a className={hamburgerContainerName} onClick={this.hamburgercross}>
                         <div className="hamburger-line hamburger-topbun"></div>
                         <div className="hamburger-line hamburger-patty"></div>
                         <div className="hamburger-line hamburger-bottombun"></div>
